405c0df2d97099783eb512fb83b9ffe31651fb5c
Reactongraph <js.developer@ongraph.com>
2024-12-22 01:12:09 +0530

    final commit of backend code

---
+++ src/api/school-information/routes/school-information.ts
@@ -0,0 +1,7 @@
+/**
+ * school-information router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::school-information.school-information');
---
+++ src/api/school-information/services/school-information.ts
@@ -0,0 +1,7 @@
+/**
+ * school-information service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::school-information.school-information');
